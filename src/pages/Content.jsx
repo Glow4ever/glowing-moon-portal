b5ec9bd48a7bb641f0c84848fa304b598bc7c9fc
Glow4ever <hector@glowingmoonmedia.com>
2026-06-07 05:20:51 -0400

    Update Content.jsx

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -109,7 +109,7 @@ export default function Content() {
 
 async function handleDownloadFolder(folder) {
     try {
-      const res = await fetch('/api/dropbox-zip', {
+      const res = await fetch('/api/dropbox_zip', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ path: folder.path_lower })
